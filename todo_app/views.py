e35e610db9e132acbf48ff53aebe98ce86ea4081
Yuhuan Fan <yfan4@binghamton.edu>
2020-03-18 23:06:06 -0700

    Create view for django todo app

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('This is the todo app homepage.')
+
+def detail(request):
+    return HttpResponse('This is the detail page of todo application')
