cae2b895e512ae347b5aa69d2e59e57ac7b93cba
Yuhuan Fan <yfan4@binghamton.edu>
2020-03-20 18:55:25 -0700

    Fix error in template url and use render function in view

--- todo_app/views.py
+++ todo_app/views.py
@@ -6,12 +6,13 @@ from django.template import loader
 # Create your views here.
 def index(request):
     task_list = Task.objects.all()
-    # output = ', '.join([q.task_name for q in task_list])
-    template = loader.get_template('todo_app/index.html')
+    # 1. output = ', '.join([q.task_name for q in task_list])
+    # 2. template = loader.get_template('todo_app/index.html')
     context = {
         'task_list': task_list
     }
-    return HttpResponse(template.render(context, request))
+    # 2. return HttpResponse(template.render(context, request))
+    return render(request, 'todo_app/index.html', context)
 
 def detail(request, task_id):
 
