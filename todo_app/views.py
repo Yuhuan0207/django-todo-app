b631c50d1de52b3626227bd997c6790530565574
Yuhuan Fan <yfan4@binghamton.edu>
2020-03-20 23:03:55 -0700

    Remove redundant code in view module

--- todo_app/views.py
+++ todo_app/views.py
@@ -6,12 +6,9 @@ from django.template import loader
 # Create your views here.
 def index(request):
     task_list = Task.objects.all()
-    # 1. output = ', '.join([q.task_name for q in task_list])
-    # 2. template = loader.get_template('todo_app/index.html')
     context = {
         'task_list': task_list
     }
-    # 2. return HttpResponse(template.render(context, request))
     return render(request, 'todo_app/index.html', context)
 
 def detail(request, task_id):
