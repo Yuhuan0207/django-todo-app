9c246bcf7c303b40034ed9e07ccb6ebde9778982
Yuhuan Fan <yfan4@binghamton.edu>
2020-03-20 19:25:56 -0700

    Name spacing for django app

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, re_path
 from todo_app import views
 
+app_name = 'todo_app'
+
 urlpatterns = [    
     re_path(r'^$', views.index, name='index'),
     re_path(r'^(?P<task_id>[0-9]+)/$', views.detail, name='detail')
