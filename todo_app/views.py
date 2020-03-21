cf5b8ec2e4d909894ccc3e6741665a99f135943f
Yuhuan Fan <yfan4@binghamton.edu>
2020-03-20 18:48:41 -0700

    Create template for Todo app

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from django.template import loader
 
 # Create your views here.
 def index(request):
     task_list = Task.objects.all()
-    output = ', '.join([q.task_name for q in task_list])
-    return HttpResponse(output)
+    # output = ', '.join([q.task_name for q in task_list])
+    template = loader.get_template('todo_app/index.html')
+    context = {
+        'task_list': task_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, task_id):
 
